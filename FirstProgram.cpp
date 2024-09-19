9c614e38a86f24c0e1cb7c0633ac81126a8c2237
NoviceBosSystems <ryangregoire286@gmail.com>
2024-09-19 13:30:17 -0400

    Add project files.

---
+++ FirstProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Have a Good Day!" << std::endl;
+	std::cout << "I am Learning C++ Development" << std::endl;
+	
+
+	system("pause > 0");
+	return 0;
+}
